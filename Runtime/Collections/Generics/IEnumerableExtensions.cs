1e3971003cef3f52615aca93e27390c9c2fe5685
Jason R. Surratt <jason.surratt@gmail.com>
2021-02-09 11:02:28 -0700

    Reorganize & add Smallest function

---
+++ Runtime/Collections/Generics/IEnumerableExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Plucky.System
+{
+    public static class IEnumerableExtensions
+    {
+        public static Random rng = new Random();
+
+        public static T Smallest<T>(this IEnumerable<T> coll, Func<T, T, int> compareTo)
+        {
+            T result = default(T);
+            bool foundOne = false;
+
+            foreach (T t in coll)
+            {
+                if (!foundOne)
+                {
+                    result = t;
+                    foundOne = true;
+                }
+                else
+                {
+                    if (compareTo(result, t) > 0)
+                    {
+                        result = t;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static T PickOne<T>(this IEnumerable<T> coll)
+        {
+            switch (coll)
+            {
+                case IList<T> list:
+                    return list[rng.Next() % list.Count];
+                default:
+                    return coll.ElementAt(rng.Next() % coll.Count());
+            }
+        }
+    }
+}
--- Runtime/IO/BinaryWriterExtensions.cs
+++ Runtime/IO/BinaryWriterExtensions.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-namespace Plucky.Common
+namespace Plucky.System
 {
     public static class BinaryWriterExtensions
     {
